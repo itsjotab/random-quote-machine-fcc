c1231edd0059be392854a2e67a7fe47ed9e6748e
jancy <jotabbusiness@icloud.com>
2022-08-10 11:35:40 -0500

    lastest upload

--- src/App.js
+++ src/App.js
@@ -21,14 +21,13 @@ function App() {
       const userPosts = await axios.get(`https://goquotes-api.herokuapp.com/api/v1/random?count=1`);
       console.log(userPosts)
       userPosts.data.quotes.map(quote => {
-        console.log('hello')
         setText(quote.text);
         setAuthor(quote.author);
       }
       )
 
     } catch (err) {
-      console.error(err.message);
+      return err.message;
     }
   };
 
@@ -50,6 +49,7 @@ function App() {
         </button>
         <button id="new-quote" className="new-quote" onClick={refreshPage}>New Quote</button>
       </div>
+      <script crossorigin src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
     </div>
   );
 }
